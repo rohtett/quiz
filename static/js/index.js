349504c61ca606dcb133ef3345ed31f0c0d3fe40
Rohit Naidu <raheet@hotmail.co.uk>
2022-05-03 11:43:23 +0100

    local storage feature for easy access to results and functioning reset button

--- static/js/index.js
+++ static/js/index.js
@@ -1,58 +1,4 @@
 "use strict";
-//declaration of variables
-const CLASSES = function() {
-	this.afflictionWarlock = 0;
-	this.arcaneMage = 0;
-	this.armsWarrior = 0;
-	this.assassinationRogue = 0;
-	this.balanceDruid = 0;
-	this.beastmasteryHunter = 0;
-	this.bloodDeathKnight = 0;
-	this.combatRogue = 0;
-	this.demonologyWarlock = 0;
-	this.destructionWarlock = 0;
-	this.disciplinePriest = 0;
-	this.elementalShaman = 0;
-	this.enhancementShaman = 0;
-	this.feralDruid = 0;
-	this.fireMage = 0;
-	this.frostDeathKnight = 0;
-	this.frostMage = 0;
-	this.furyWarrior = 0;
-	this.holyPaladin = 0;
-	this.holyPriest = 0;
-	this.marksmanshipHunter = 0;
-	this.protectionPaladin = 0;
-	this.protectionWarrior = 0;
-	this.restorationDruid = 0;
-	this.restorationShaman = 0
-	this.retributionPaladin = 0;
-	this.shadowPriest = 0;
-	this.subtletyRogue = 0;
-	this.survivalHunter = 0;
-	this.unholyDeathKnight = 0;
-}
-var counter = new CLASSES();
-const OPTIONS = document.querySelectorAll("#options input");
-//"next" function for instructional page
-let instructionNext = function() {
-	if (
-		OPTIONS[0].checked===true&&
-		OPTIONS[1].checked===true&&
-		OPTIONS[2].checked===true&&
-		OPTIONS[3].checked===true
-	) {
-		//question number tracker
-		var progress = 1;
-		sessionStorage.setItem("progress", progress);
-		//tracker for the answer from each question
-		sessionStorage.setItem(1, undefined);
-		sessionStorage.setItem(2, undefined);
-		sessionStorage.setItem(3, undefined);
-		sessionStorage.setItem(4, undefined);
-		window.location.href = 'wow-class-quiz-1.html';
-	}
-}
 //question one answer one
 var questionCount = 1;
 let playstyleCareful = function() {
@@ -63,19 +9,16 @@ let playstyleCareful = function() {
 	counter.subtletyRogue +=2;
 	counter.marksmanshipHunter +=2;
 	counter.demonologyWarlock +=2;
-
 	counter.feralDruid +=1;
 	counter.restorationShaman +=1;
 	counter.balanceDruid +=1;
 	counter.frostMage +=1;
-
 	counter.survivalHunter -=1;
 	counter.protectionPaladin -=1;
 	counter.bloodDeathKnight -=1;
 	counter.protectionWarrior -=1;
 	counter.enhancementShaman -=1;
 	counter.disciplinePriest -=1;
-
 	counter.arcaneMage -=2;
 	counter.combatRogue -=2;
 	counter.furyWarrior -=2;
--- static/js/wow-class-quiz.js
+++ static/js/wow-class-quiz.js
@@ -1,24 +1,110 @@
 "use strict";
 window.onload = function() {
+	if (localStorage.state == "submit") {
+		localStorage.setItem("state", "parse");
+		window.location.href = "result.html";
+	}
+	if (localStorage.state == "parse") {
+		let results = JSON.parse(localStorage.result);
+		let resultValues = [];
+		for (var value in results) {
+			resultValues.push([value, results[value]]);
+		}
+		resultValues.sort(function(a, b) {
+			return b[1] - a[1];
+		});
+		console.log(resultValues);
+		setTimeout(function() {
+			localStorage.setItem("state", "submit");
+		}, 25);
+	}
+}	
+//"next" function for instructional page
+function instructionNext() {
+	if (
+		OPTIONS[0].checked===true&&
+		OPTIONS[1].checked===true&&
+		OPTIONS[2].checked===true&&
+		OPTIONS[3].checked===true
+	) {
+		restart();
+	} else {
+		OPTIONS[3].focus();
+		document.getElementById("error").style.visibility = "visible";
+	}		
 }
-
-let next = function() {
+function restart() {
+	sessionStorage.clear();
+	localStorage.clear();
+	//question number tracker
+	var progress = 1;
+	sessionStorage.setItem("progress", progress);
+	//tracker for the answer from each question
+	sessionStorage.setItem(1, undefined);
+	sessionStorage.setItem(2, undefined);
+	sessionStorage.setItem(3, undefined);
+	sessionStorage.setItem(4, undefined);
+	window.location.href = 'wow-class-quiz-1.html';
+}
+	
+function next() {
 	for (let i = 0; i < OPTIONS.length; i++) {
 		if (OPTIONS[i].checked===true) {
 			let progress = parseInt(sessionStorage.progress);
 			let question = sessionStorage.getItem(progress)
-			console.log(question)
 			question = OPTIONS[i].id+"()";
 			sessionStorage.setItem(progress, question);
 			progress += 1;
 			if (progress < sessionStorage.length) {
 				sessionStorage.setItem("progress", progress)
 				window.location.href = `wow-class-quiz-${progress}.html`;
+				break
 			} else {
-				console.log(sessionStorage.getItem(1));
+				eval(sessionStorage.getItem(1));
+				//eval(sessionStorage.getItem(2));
+				//eval(sessionStorage.getItem(3));
+				//eval(sessionStorage.getItem(4));
+				localStorage.setItem("result", JSON.stringify(counter));
+				localStorage.setItem("state", "submit");
 				window.location.href = "result.html";
 			}
-			break
+		} else {
+			document.getElementById("error").style.visibility = "visible";
 		}
 	}
 }
+//declaration of variables
+const CLASSES = function() {
+	this.afflictionWarlock = 0;
+	this.arcaneMage = 0;
+	this.armsWarrior = 0;
+	this.assassinationRogue = 0;
+	this.balanceDruid = 0;
+	this.beastmasteryHunter = 0;
+	this.bloodDeathKnight = 0;
+	this.combatRogue = 0;
+	this.demonologyWarlock = 0;
+	this.destructionWarlock = 0;
+	this.disciplinePriest = 0;
+	this.elementalShaman = 0;
+	this.enhancementShaman = 0;
+	this.feralDruid = 0;
+	this.fireMage = 0;
+	this.frostDeathKnight = 0;
+	this.frostMage = 0;
+	this.furyWarrior = 0;
+	this.holyPaladin = 0;
+	this.holyPriest = 0;
+	this.marksmanshipHunter = 0;
+	this.protectionPaladin = 0;
+	this.protectionWarrior = 0;
+	this.restorationDruid = 0;
+	this.restorationShaman = 0
+	this.retributionPaladin = 0;
+	this.shadowPriest = 0;
+	this.subtletyRogue = 0;
+	this.survivalHunter = 0;
+	this.unholyDeathKnight = 0;
+}
+var counter = new CLASSES();
+const OPTIONS = document.querySelectorAll("#options input");
