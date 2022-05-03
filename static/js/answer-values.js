3b3b99f58f2942dcabe636734e7de83a38ddbbe3
Rohit Naidu <raheet@hotmail.co.uk>
2022-05-03 02:56:03 +0100

    added counters for calculation of question as well as some basic testing for counter objects

---
+++ static/js/answer-values.js
@@ -0,0 +1,29 @@
+"use strict";
+let playstyleCareful = function() {
+	counter.afflictionWarlock+=2;
+	counter.shadowPriest+=2;
+	counter.restorationDruid +=2;
+	counter.holyPriest +=2;
+	counter.subtletyRogue +=2;
+	counter.marksmanshipHunter +=2;
+	counter.demonologyWarlock +=2;
+
+	counter.feralDruid +=1;
+	counter.restorationShaman +=1;
+	counter.balanceDruid +=1;
+	counter.frostMage +=1;
+
+	counter.survivalHunter -=1;
+	counter.protectionPaladin -=1;
+	counter.bloodDeathKnight -=1;
+	counter.protectionWarrior -=1;
+	counter.enhancementShaman -=1;
+	counter.disciplinePriest -=1;
+
+	counter.arcaneMage -=2;
+	counter.combatRogue -=2;
+	counter.furyWarrior -=2;
+	counter.retributionPaladin -=2;
+	counter.elementalShaman -=2;
+	counter.frostDeathKnight -=2;
+}
--- static/js/index.js
+++ static/js/index.js
@@ -1,1 +1,40 @@
 "use strict";
+const CLASSES = function() {
+	this.afflictionWarlock = 0;
+	this.arcaneMage = 0;
+	this.armsWarrior = 0;
+	this.assassinationRogue = 0;
+	this.balanceDruid = 0;
+	this.beastmasteryHunter = 0;
+	this.bloodDeathKnight = 0;
+	this.combatRogue = 0;
+	this.demonologyWarlock = 0;
+	this.destructionWarlock = 0;
+	this.disciplinePriest = 0;
+	this.feralDruid = 0;
+	this.fireMage = 0;
+	this.frostDeathKnight = 0;
+	this.frostMage = 0;
+	this.furyWarrior = 0;
+	this.holyPaladin = 0;
+	this.holyPriest = 0;
+	this.marksmanshipHunter = 0;
+	this.protectionPaladin = 0;
+	this.protectionWarrior = 0;
+	this.restorationDruid = 0;
+	this.retributionPaladin = 0;
+	this.shadowPriest = 0;
+	this.subtletyRogue = 0;
+	this.survivalHunter = 0;
+	this.unholyDeathKnight = 0;
+}
+var counter = new CLASSES();
+let question = [
+	undefined,
+	undefined,
+	undefined,
+	undefined,
+];
+window.onload = function() {
+	question[0] = playstyleCareful;
+}
